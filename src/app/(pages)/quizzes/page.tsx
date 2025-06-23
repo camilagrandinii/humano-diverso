381cc373929084984e604a17fd1d1a6101872c16
camilagrandinii <cacagrandini@gmail.com>
2025-06-23 13:39:11 +0200

    [CG] feat: added modules to empty routes in order to avoid build errors

---
+++ src/app/(pages)/quizzes/page.tsx
@@ -0,0 +1,12 @@
+
+// app/page.tsx
+
+import HeroSection from '@/features/home/components/HeroSection';
+
+export default function Quizzes() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
---
+++ src/app/(pages)/quizzes/result/page.tsx
@@ -0,0 +1,12 @@
+
+// app/page.tsx
+
+import HeroSection from '@/features/home/components/HeroSection';
+
+export default function Result() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
