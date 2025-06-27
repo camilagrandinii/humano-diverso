688f5478564b3a6e2ca266dc9b06cfc7750f5c6d
camilagrandinii <cacagrandini@gmail.com>
2025-06-27 04:37:13 +0200

    [CG] fix: commented pdf usage

--- src/app/(pages)/understand-diversity/lgbt/page.tsx
+++ src/app/(pages)/understand-diversity/lgbt/page.tsx
@@ -1,11 +1,10 @@
 import Lgbt from './lgbt';
-import MyPDF from './pdf';
 
 export default function HomePage() {
   return (
     <main>
       <Lgbt />
-      <MyPDF />
+      {/* <MyPDF /> */}
     </main>
   );
 }
