da6624666aea659d9d52c0f58ab8035152a58237
camilagrandinii <cacagrandini@gmail.com>
2025-06-27 04:26:28 +0200

    [CG] feat: added content related to negritude and completed info regarding lgbt's and feminism

--- src/app/(pages)/understand-diversity/negritude/negritude.tsx
+++ src/app/(pages)/understand-diversity/negritude/negritude.tsx
@@ -3,36 +3,11 @@
 import TopicInfo
   from '@/features/understand-diversity/components/TopicInformation';
 
-import rawData
+import data
   from '../../../../features/understand-diversity/data/negritude.json';
 
-const transformData = (data: any) => {
-  const sections = [];
-
-  let index = 1;
-  while (true) {
-    const title = data[`title${index === 1 ? '' : `-${index}`}`];
-    const headline = data[`headline${index === 1 ? '' : `-${index}`}`];
-    const content = data[`content${index === 1 ? '' : `-${index}`}`];
-    const images = data[`images${index === 1 ? '' : `-${index}`}`];
-
-    if (!title || !headline || !content || !images) break;
-
-    sections.push({ title, headline, content, images });
-    index++;
-  }
-
-  return {
-    sections,
-    cta: data.cta,
-    centerText: data.centerText,
-    references: data.references,
-  };
-};
-
-const Negritude = () => {
-  const data = transformData(rawData);
+const Lgbt = () => {
   return <TopicInfo {...data} />;
 };
 
-export default Negritude;
+export default Lgbt;
